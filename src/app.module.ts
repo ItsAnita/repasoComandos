1b89daa3e85c9dda0117134abaa1eea6ebd4105e
ItsAnita <aant25220@gmail.com>
2024-10-17 09:57:09 -0600

    resource example

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TownsModule } from './towns/towns.module';
 import { AnnouncementModule } from './announcement/announcement.module';
 import { PlaceModule } from './place/place.module';
+import { ExampleModule } from './example/example.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { PlaceModule } from './place/place.module';
     TownsModule,
     AnnouncementModule,
     PlaceModule,
+    ExampleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
